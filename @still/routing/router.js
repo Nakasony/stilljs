26b57596b007b75b22ad0101f6894688274852ef
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-09-14 16:51:55 +0100

    Clean up on th router component Adjusting component loading/reloading form the router

--- @still/routing/router.js
+++ @still/routing/router.js
@@ -2,29 +2,31 @@ class Router {
 
     static goto(cmp){
 
-        const appCntrId = 'appPlaceholder';
-        const routeInstance = getRouteMap()
+        const applicationContainerId = 'appPlaceholder';
+        /**
+         * getRouteMap() function stated inside route.map.js in the root of the project
+         */
+        const routeInstance = getRouteMap();
         const route = routeInstance.route[cmp];
 
         loadComponentFromPath(route, cmp)
         .then(({ imported }) => {
-            if(imported) {
-                delete $still.context.componentRegistror.componentList[cmp];
-            };
-            $still.context.currentView = eval(`new ${cmp}()`);
-            const newElm = document.createElement('div');
-            newElm.onload = function(){
-                console.log(`ELEMENT LOADED SUCCESS`);
+            if(!imported) {
+                //delete $still.context.componentRegistror.componentList[cmp];
+                $still.context.currentView = eval(`new ${cmp}()`);
+            }else{
+                $still.context.currentView = $still.component.list[cmp];
             }
+
             /**
              * the bellow line clears previous component from memory
              * @type { ViewComponent }
              */
             const componentInstance = $still.context.currentView;
-            const appPlaceholder = document.getElementById(appCntrId);
-            
-            appPlaceholder.innerHTML = ''
-            appPlaceholder.insertAdjacentHTML('afterbegin', componentInstance.getTemplate());
+            //componentInstance.
+            document
+                .getElementById(applicationContainerId)
+                .innerHTML = componentInstance.getTemplate();
             componentInstance.onRender();
         });
 
